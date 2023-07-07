7b6230d0f453e28330da871f96d0b82d7a1362b0
rish <kjeremyth@yandex.com>
2023-07-08 00:26:32 +0530

    update formatting

--- src/rish/scripts/challenges/hackerearth/EqualString.java
+++ src/rish/scripts/challenges/hackerearth/EqualString.java
@@ -5,64 +5,65 @@ import java.util.ArrayList;
 import java.util.List;
 
 /*
- * Problem link:
- * https://www.hackerearth.com/challenges/competitive/june-circuits-23/algorithm
- * /equal-strings-79789662-4dbd707c/
  * 
+ * Contest link: https://www.hackerearth.com/challenges/competitive/june-circuits-23/problems/
+ * Problem link: https://www.hackerearth.com/practice/algorithms/searching/linear-search/practice-problems/algorithm/equal-strings-79789662-4dbd707c/
+ * 
+ * @Author: Rishabh Soni
  * Date attempted: 20th June 2023
+ * 
  */
-
 public class EqualString {
 
-	public static int canManipulate(String a, String b, int length, int cost) {
+    public static int canManipulate(String a, String b, int length, int cost) {
 
-		char[] refArray = a.toCharArray();
-		char[] diffArray = b.toCharArray();
+        char[] refArray = a.toCharArray();
+        char[] diffArray = b.toCharArray();
 
-		List<Integer> indexes = new ArrayList<>();
+        List<Integer> indexes = new ArrayList<>();
 
-		for (int i = 0; i < length; i++) {
-			if (refArray[i] != diffArray[i]) {
-				indexes.add(i);
-			}
-		}
+        for (int i = 0; i < length; i++) {
+            if (refArray[i] != diffArray[i]) {
+                indexes.add(i);
+            }
+        }
 
-		if (indexes.size() % 2 != 0) {
-			return -1;
-		}
+        if (indexes.size() % 2 != 0) {
+            return -1;
+        }
 
-		int totalCost = 0;
-		for (int i = 0; i < indexes.size(); i = i + 2) {
-			int indexDiff = indexes.get(i + 1) - indexes.get(i);
-			totalCost += indexDiff < cost ? indexDiff : cost;
-		}
+        int totalCost = 0;
+        for (int i = 0; i < indexes.size(); i = i + 2) {
+            int indexDiff = indexes.get(i + 1) - indexes.get(i);
+            totalCost += indexDiff < cost ? indexDiff : cost;
+        }
 
-		return totalCost;
-	}
+        return totalCost;
+    }
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
+    public static void main(String[] args) throws NumberFormatException, IOException {
 
-		/*
-		 * BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); int
-		 * testCaseCount = Integer.parseInt(br.readLine());
-		 * 
-		 * while (testCaseCount > 0) {
-		 * 
-		 * String sizes = br.readLine(); String[] strs = sizes.trim().split(" "); int
-		 * length = Integer.parseInt(strs[0]); int cost = Integer.parseInt(strs[1]);
-		 * 
-		 * String nextLine = br.readLine(); String[] inputStrings =
-		 * nextLine.trim().split(" ");
-		 * 
-		 * System.out.println(canManipulate(inputStrings[0], inputStrings[1], length,
-		 * cost));
-		 * 
-		 * testCaseCount--; }
-		 */
+        /*
+         * BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); int
+         * testCaseCount = Integer.parseInt(br.readLine());
+         * 
+         * while (testCaseCount > 0) {
+         * 
+         * String sizes = br.readLine(); String[] strs = sizes.trim().split(" "); int
+         * length = Integer.parseInt(strs[0]); int cost = Integer.parseInt(strs[1]);
+         * 
+         * String nextLine = br.readLine(); String[] inputStrings =
+         * nextLine.trim().split(" ");
+         * 
+         * System.out.println(canManipulate(inputStrings[0], inputStrings[1], length,
+         * cost));
+         * 
+         * testCaseCount--; }
+         */
 
-		System.out.println(canManipulate("1010", "0001", 4, 3));
-		System.out.println(canManipulate("101011", "010010", 6, 2));
-		System.out.println(canManipulate("001111011011", "010101101111", 12, 2));
-	}
+        System.out.println(canManipulate("1010", "0001", 4, 3));
+        System.out.println(canManipulate("101011", "010010", 6, 2));
+        System.out.println(canManipulate("001111011011", "010101101111", 12, 2));
+    }
 
 }
--- src/rish/scripts/challenges/hackerearth/NoInversions.java
+++ src/rish/scripts/challenges/hackerearth/NoInversions.java
@@ -3,12 +3,10 @@ package rish.scripts.challenges.hackerearth;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
+/*
  * 
- * Contest link:
- * https://www.hackerearth.com/challenges/competitive/june-circuits-23/problems/
- * Problem link:
- * https://www.hackerearth.com/practice/data-structures/advanced-data-structures/segment-trees/practice-problems/algorithm/no-inversions-db0ebea5/
+ * Contest link: https://www.hackerearth.com/challenges/competitive/june-circuits-23/problems/
+ * Problem link: https://www.hackerearth.com/practice/data-structures/advanced-data-structures/segment-trees/practice-problems/algorithm/no-inversions-db0ebea5/
  * 
  * Date attempted: 30th June 2023
  * 
@@ -17,100 +15,100 @@ import java.util.List;
  */
 public class NoInversions {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		String test = "abced";
+        String test = "abced";
 
-		StringSegmentTree tree = new StringSegmentTree(test);
-		tree.printTree();
-		System.out.println(tree.query(1, 4));
+        StringSegmentTree tree = new StringSegmentTree(test);
+        tree.printTree();
+        System.out.println(tree.query(1, 4));
 
-		System.out.println(isInverted("abcd"));
-	}
+        System.out.println(isInverted("abcd"));
+    }
 
-	public static boolean isInverted(String s) {
-		int init = 0;
-		for (char c : s.toCharArray()) {
-			int cVal = Character.getNumericValue(c);
-			if (cVal < init) {
-				return true;
-			}
-			init = cVal;
-		}
-		return false;
-	}
+    public static boolean isInverted(String s) {
+        int init = 0;
+        for (char c : s.toCharArray()) {
+            int cVal = Character.getNumericValue(c);
+            if (cVal < init) {
+                return true;
+            }
+            init = cVal;
+        }
+        return false;
+    }
 
 }
 
 class StringSegmentTree {
 
-	String input;
-	String[] tree;
-	int[] invTree;
+    String input;
+    String[] tree;
+    int[] invTree;
 
-	public StringSegmentTree(String input) {
+    public StringSegmentTree(String input) {
 
-		int size = input.length();
-		int height = (int) (Math.ceil(Math.log(size) / Math.log(2)));
-		int maxSize = 2 * (int) Math.pow(2, height) - 1;
+        int size = input.length();
+        int height = (int) (Math.ceil(Math.log(size) / Math.log(2)));
+        int maxSize = 2 * (int) Math.pow(2, height) - 1;
 
-		this.input = input;
-		this.tree = new String[maxSize];
-		this.invTree = new int[maxSize];
-		buildInternal(0, size - 1, 0);
-	}
+        this.input = input;
+        this.tree = new String[maxSize];
+        this.invTree = new int[maxSize];
+        buildInternal(0, size - 1, 0);
+    }
 
-	public List<String> query(int i, int j) {
-		List<String> answer = new ArrayList<>();
-		queryInternal(answer, 0, 0, input.length() - 1, i, j);
-		return answer;
-	}
+    public List<String> query(int i, int j) {
+        List<String> answer = new ArrayList<>();
+        queryInternal(answer, 0, 0, input.length() - 1, i, j);
+        return answer;
+    }
 
-	private String queryInternal(List<String> answer, int node, int rangeLeft, int rangeRight, int i, int j) {
+    private String queryInternal(List<String> answer, int node, int rangeLeft, int rangeRight, int i, int j) {
 
-		if (i <= rangeLeft && j >= rangeRight) {
+        if (i <= rangeLeft && j >= rangeRight) {
 //			answer.add(tree[node]);
-			// return NoInversions.isInverted(tree[node]) ? 1 : 0;
-			System.out.println(tree[node]);
-			return tree[node];
-		}
+            // return NoInversions.isInverted(tree[node]) ? 1 : 0;
+            System.out.println(tree[node]);
+            return tree[node];
+        }
 
-		if (rangeRight < i || rangeLeft > j) {
-			return "";
-		}
+        if (rangeRight < i || rangeLeft > j) {
+            return "";
+        }
 
-		int mid = (rangeLeft + rangeRight) / 2;
-		int leftChild = 2 * node + 1;
-		int rightChild = leftChild + 1;
+        int mid = (rangeLeft + rangeRight) / 2;
+        int leftChild = 2 * node + 1;
+        int rightChild = leftChild + 1;
 
-		String a = queryInternal(answer, leftChild, rangeLeft, mid, i, j);
-		String b = queryInternal(answer, rightChild, mid + 1, rangeRight, i, j);
-		System.out.println(a + ":" + b);
-		return a + b;
-	}
+        String a = queryInternal(answer, leftChild, rangeLeft, mid, i, j);
+        String b = queryInternal(answer, rightChild, mid + 1, rangeRight, i, j);
+        System.out.println(a + ":" + b);
+        return a + b;
+    }
 
-	private String buildInternal(int i, int j, int node) {
+    private String buildInternal(int i, int j, int node) {
 
-		if (i == j) {
-			tree[node] = input.substring(i, j + 1);
-			invTree[node] = 1;
-			return tree[node];
-		}
+        if (i == j) {
+            tree[node] = input.substring(i, j + 1);
+            invTree[node] = 1;
+            return tree[node];
+        }
 
-		int mid = (i + j) / 2;
-		int leftChild = 2 * node + 1;
-		int rightChild = leftChild + 1;
+        int mid = (i + j) / 2;
+        int leftChild = 2 * node + 1;
+        int rightChild = leftChild + 1;
 
-		System.out.println(input.substring(i, j + 1));
-		tree[node] = buildInternal(i, mid, leftChild) + buildInternal(mid + 1, j, rightChild);
-		return tree[node];
-	}
+        System.out.println(input.substring(i, j + 1));
+        tree[node] = buildInternal(i, mid, leftChild) + buildInternal(mid + 1, j, rightChild);
+        return tree[node];
+    }
 
-	public void printTree() {
-		for (int i = 0; i < tree.length; i++) {
-			System.out.print(tree[i] + " ");
-		}
-		System.out.println();
-	}
+    public void printTree() {
+        for (int i = 0; i < tree.length; i++) {
+            System.out.print(tree[i] + " ");
+        }
+        System.out.println();
+    }
 
 }
